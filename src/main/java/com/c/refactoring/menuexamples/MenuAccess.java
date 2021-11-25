c958589ec37827d1fda1f8c392fa36f018fa7869
Pravin Tripathi <pravinyo12@gmail.com>
2021-11-25 11:45:54 +0530

    simplified

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -4,31 +4,32 @@ import java.util.List;
 
 public class MenuAccess {
 
-    public void setAuthorizationsInEachMenus(
-            List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
+    public void setAuthorizationsInEachMenus(List<MenuItem> menuItems, Role[] roles) {
+        if (roles == null) return;
+
+        for (MenuItem menuItem : menuItems) {
+            for (Role role : roles) {
+                if (isReadAccess(menuItem, role)) {
+                    assignAccess(menuItem, Constants.READ);
+                } else if (isWriteAccess(menuItem, role)) {
+                    assignAccess(menuItem, Constants.WRITE);
                 }
-
             }
-
         }
+    }
 
+    private boolean isReadAccess(MenuItem menuItem, Role role) {
+        return role.getName().equals(menuItem.getReadAccessRole())
+                && !Constants.WRITE.equals(menuItem.getAccess());
+    }
+
+    private boolean isWriteAccess(MenuItem menuItem, Role role) {
+        return role.getName().equals(menuItem.getWriteAccessRole());
+    }
+
+    private void assignAccess(MenuItem menuItem, String type) {
+        menuItem.setAccess(type);
+        menuItem.setVisible(true);
     }
 
 }
